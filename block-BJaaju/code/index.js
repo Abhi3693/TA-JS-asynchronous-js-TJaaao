286e40f745bf27e8de1be1e9f5c66ee22f979a29
Abhijeet Mane <abhi@Varshas-MacBook-Air.local>
2022-01-12 20:53:21 +0530

    Added change in search image app

--- block-BJaaju/code/index.js
+++ block-BJaaju/code/index.js
@@ -1,25 +1,42 @@
 let ul = document.querySelector("ul");
 let input = document.querySelector("input");
 
+const url = `https://api.unsplash.com/photos/?client_id=uKVp02dxQxC8uddVoHKxEEo23WTT7But75Mgf7dMVHQ`;
+
+const searchUrl = (querry)=> {
+    let searchFor = `https://api.unsplash.com/search/photos?query=${querry}&client_id=uKVp02dxQxC8uddVoHKxEEo23WTT7But75Mgf7dMVHQ`;
+    return searchFor;
+} 
+
+function fetch(url, successHandler) {
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", url);
+    xhr.onload = ()=> successHandler(JSON.parse(xhr.response));
+    xhr.onerror = ()=> console.log("Something Went wrong");
+    xhr.send();
+}
+
+function createImages(images) {
+    ul.innerHTML = ""
+    images.forEach((elm)=> {
+        let li = document.createElement("li");
+        let img = document.createElement("img");
+        img.src = elm.urls.small;
+        img.style.width = "400px";
+        img.style.height = "400px";
+        li.append(img);
+        ul.append(li);
+    })
+}
+
 function handleInput(event){
-    ul.innerHTML = "";
-    if(event.keyCode === 13){
-        let xhr = new XMLHttpRequest();
-        xhr.open("GET", `https://api.unsplash.com/search/collections/?client_id=ppyMHhcLdYZE7vLCZaKd7PE4ReYCTo8yyK9BcWYmgPg&query=${input.value}`)
-        xhr.onload = function(){
-           let user = JSON.parse(xhr.response);
-           user.results.forEach((elm)=> {
-                let li = document.createElement("li");
-                let img = document.createElement("img");
-                img.src = elm.cover_photo.urls.small;
-                li.append(img);
-                ul.append(li);
-           })
-        }
-        xhr.onerror = ()=> console.log("Something Went wrong");
-        xhr.send();
-        
+    if(event.keyCode === 13 && input.value){
+        fetch(searchUrl(event.target.value), (images)=> {
+            createImages(images.results)
+        });
+        input.value = "";
     }
 }
 
+fetch(url,createImages);
 input.addEventListener("keyup", handleInput)
